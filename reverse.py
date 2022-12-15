0914076949cdfedfd1f3a0446d812abb9f45e785
Jacobus Burger <therealjacoburger@gmail.com>
2022-12-15 10:38:42 -0800

    extended python reverse

--- reverse.py
+++ reverse.py
@@ -1,3 +1,13 @@
+# Jacobus Burger (2022)
+# Info:
+#   Python equivalent of the C program by the same name
+
+
 # I love snakelang
-print(input()[::-1])  # reverse any string
-[i for i in range(1, 17)][::-1]  # reverse an array/list of integers (equivalent of C program)
+def reverse(array):
+    return array[::-1]
+
+
+if __name__ == '__main__':
+    array = [i for i in range(1, 17)]
+    print(array, reverse(array))
