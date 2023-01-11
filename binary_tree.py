3d8ed182caa9be0db500610609fe4e32a4ffac08
Jacobus Burger <therealjacoburger@gmail.com>
2023-01-10 17:14:01 -0800

    added binary heap tree implementation

--- binary_tree.py
+++ binary_tree.py
@@ -9,3 +9,42 @@ class BTree:
         self.data = data
         self.left = left
         self.right = right
+
+
+
+
+
+# my preferred solution: a binary heap tree
+from math import ceil, floor, log
+class HeapTree:
+    def __init__(self, *data):
+        self.index = 0
+        if len(data) > 0:
+            # create a complete tree of 2**h nodes
+            self.height = ceil(log(len(data)) / log(2))
+            self.tree = [
+                data[i]
+                if i < len(data)
+                else None
+                for i in range(2**self.height)
+            ]
+        else:
+            self.tree = [None]
+            self.height = 1
+
+
+    def __repr__(self):
+        return str(self.tree)
+
+
+    def parent(self):
+        if self.index > 0:
+            self.index = floor((self.index - 1) / 2)
+
+
+    def left(self):
+        self.index = 2 * self.index + 1
+
+
+    def right(self):
+        self.index = 2 * self.index + 2
