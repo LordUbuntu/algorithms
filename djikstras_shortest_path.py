995650a41d86ab9b78abd5ce34036fc038fbc25e
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-12 20:12:12 -0800

    algorithm sort of working

--- djikstras_shortest_path.py
+++ djikstras_shortest_path.py
@@ -2,11 +2,11 @@
 
 # should go A -> B -> C -> F for total weight of 4 (< 5 or 6)
 G = {
-    'A': [['B', 1], ['E', 6]],
+    'A': [['B', 1]],
     'B': [['A', 1], ['C', 1], ['D', 3]],
     'C': [['B', 1], ['E', 2]],
     'D': [['B', 3], ['E', 1]],
-    'E': [['A', 6], ['C', 2], ['D', 1]]
+    'E': [['C', 2], ['D', 1]]
 }
 
 # or as dict comprehension with input of form:
@@ -16,19 +16,18 @@ G = {
 
 
 def djikstra(G, source, sink):
-    unvisited = [[0, source]]
+    unvisited = [[source, 0]]
     visited = []
 
     while unvisited:
-        v, cost = unvisited.pop()
+        v, cost = unvisited.pop(0)
         print(unvisited, visited, v, cost)
-        if v in visited:
-            continue
-        visited.append(v)
-        if v == sink:
-            return cost
-        for w, c in G[v]:
-            if w in visited:
-                continue
-            unvisited.append([w, cost + c])
+        if v not in visited:
+            visited.append(v)
+            for w, c in G[v]:
+                if w in visited:
+                    continue
+                unvisited.append([w, cost + c])
+            if v == sink:
+                return cost
     return [None, float("inf")]
