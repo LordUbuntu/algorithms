65b6b0aa9c86030a99d0605343ab3510cb235547
Jacobus Burger <therealjacoburger@gmail.com>
2023-02-14 23:48:31 -0800

    added terminals trick

--- random_graph.py
+++ random_graph.py
@@ -7,3 +7,14 @@ G = {
     4: 2,
     5: 3
 }
+
+
+# find terminal vertices (by which vertices of G have in-deg of 0)
+#   working principle: left side of : are what are pointing (from),
+#   while right side of : are what are being pointed at (to). Ergo, if 
+#   something is pointing (in from) but not being pointed at (in to),
+#   then it is a terminal vertex, assuming a convergent digraph...
+def terminals(G):
+    to = G.values()
+    from = G.keys()
+    return list(filter(lambda v: v not in to, from))
