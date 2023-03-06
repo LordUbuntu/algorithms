46e3a9147a54ff6fe0585a62bca7f12d79ed6e28
Jacobus Burger <therealjacoburger@gmail.com>
2023-03-05 17:49:11 -0800

    added binary search, one of the classic algorithms

---
+++ binary_search.py
@@ -0,0 +1,14 @@
+# Jacobus Burger (2023)
+# binary search on an array of values
+# see: https://en.wikipedia.org/wiki/Binary_search_algorithm
+def search(array, target):
+    left, right = 0, len(array) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if target == nums[mid]:
+            return mid
+        elif target >= nums[mid]:
+            left = mid + 1
+        elif target < nums[mid]:
+            right = mid - 1
+    return -1
