d193c8f12b66188e1c9ff1abe59ae4cc24da4e5b
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-04 15:12:39 -0700

    added other *gram algorithms

--- annagram.py
+++ annagram.py
@@ -1,7 +1,16 @@
 # Jacobus Burger (2023)
 # Info:
-#   Algorithm to check if a string is an annagram of another
-def anna(original, string):
-    if sorted(original) == sorted(string):
+#   Algorithms to compare different arrangements of letters in a string
+
+# was "annagram" the term for a string that can arranged into another (not necessarily read the same forwards as backwards)
+def same_letters(a, b):
+    if sorted(a) == sorted(b):
+        return True
+    return False
+
+
+# what was the term for a string that is the reversed version of another?
+def same_reversed(a, b):
+    if a == reversed(b):
         return True
     return False
