c85e08582769f0045bf75f363df3263402e5ccd4
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-06 22:48:48 -0700

    added demo for binary search

--- binary_search.c
+++ binary_search.c
@@ -1,25 +1,28 @@
 // Jacobus Burger (2023)
 // binary search on an array of values, O(log n) time
 // see: https://en.wikipedia.org/wiki/Binary_search_algorithm
-int
-search(int target, int *array, int length)
-{
+#include <stdio.h>
+
+
+int search(int target, int *array, int length) {
         int left = 0, right = length - 1;
-        while (left <= right)
-        {
+        while (left <= right) {
                 int middle = (left + right) / 2;
-                if (target == array[middle])
-                {
+                if (target == array[middle]) {
                         return middle;
-                }
-                else if (target > array[middle])
-                {
+                } else if (target > array[middle]) {
                         left = middle + 1;
-                }
-                else if (target < array[middle])
-                {
+                } else if (target < array[middle]) {
                         right = middle - 1;
                 }
         }
         return -1;
+}
+
+
+int main(void) {
+        int arr[5] = {1, 3, 4, 7, 8};
+        printf("target: %i, result: %i\n", 3, search(3, arr, 5));
+        printf("target: %i, result: %i\n", 10, search(10, arr, 5));
+        printf("target: %i, result: %i\n", 10, search(10, arr, 5));
 }
