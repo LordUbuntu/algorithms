7072efdb02640fe1d34e3434ac09ac685fc92021
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-11 22:09:50 -0700

    added early quit shortcut

--- bubble_sort.py
+++ bubble_sort.py
@@ -4,6 +4,12 @@
 # if they're out of order. It's worst case is O(n^2), not great.
 def sort(l: list) -> list:
     for _ in range(1, len(l)):
+        sorted = False
         for index in range(1, len(l)):
             if l[index - 1] >= l[index]:
+                sorted = False
                 l[index - 1], l[index] = l[index], l[index - 1]
+            else:
+                sorted = True
+        if sorted == True:
+            break
