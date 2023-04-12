f5d24383c5e604482fd124ad27340531bc37e1ee
Jacobus Burger <therealjacoburger@gmail.com>
2023-04-11 22:14:00 -0700

    changed sort to return a sorted list instead of modifying original list

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,13 +3,15 @@
 # Bubble sort compaires pairs of adjacent values and swaps them
 # if they're out of order. It's worst case is O(n^2), not great.
 def sort(l: list) -> list:
-    for _ in range(1, len(l)):
+    result = l.copy()
+    for _ in range(1, len(result)):
         sorted = False
-        for index in range(1, len(l)):
-            if l[index - 1] >= l[index]:
+        for index in range(1, len(result)):
+            if result[index - 1] >= result[index]:
                 sorted = False
-                l[index - 1], l[index] = l[index], l[index - 1]
+                result[index - 1], result[index] = result[index], result[index - 1]
             else:
                 sorted = True
         if sorted == True:
             break
+    return result
