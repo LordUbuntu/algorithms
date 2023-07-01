89e93e0fa65d3ddbb92b0796e406b4e5c9b0d2af
LordUbuntu <therealjacoburger@gmail.com>
2023-07-01 11:48:32 -0700

    added bogosort (the best sort)

---
+++ bogo_sort.py
@@ -0,0 +1,16 @@
+from random import randint as rand
+from time import sleep
+
+
+def bogo(arr: list) -> list:
+    while arr != sorted(arr):
+        for i in range(len(arr)):
+            pick = rand(0, len(arr) - 1)
+            arr[i], arr[pick] = arr[pick], arr[i]
+        print(arr)
+        sleep(1)
+    return arr
+
+
+if __name__ == "__main__":
+    bogo([3, 5, 4, 2, 1])
