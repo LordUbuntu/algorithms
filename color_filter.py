9280ccdca8be902159db0704780a6867c29c809e
LordUbuntu <therealjacoburger@gmail.com>
2023-08-18 17:33:29 -0700

    added color mutation method

--- color_filter.py
+++ color_filter.py
@@ -39,3 +39,16 @@ def saturation(image_path: str, output_path = "output.jpg", value = 1):
     image = Image.open(image_path)
     out = ImageEnhance.Color(image).enhance(value)
     out.save(output_path)
+
+
+def mutate(image_path: str, output_path = "output.jpg", color = (0, 0, 0)):
+    image = Image.open(image_path)
+
+    for pixel in product(range(image.width), range(image.height)):
+        old_color = image.getpixel(pixel)
+        new_color = (old_color[0] + color[0], old_color[1] + color[1], old_color[2] + color[2])
+        image.putpixel(pixel, new_color)
+
+    image.save(output_path)
+
+
