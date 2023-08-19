a64d02fd6e38d974ad68d8ce1076895c48e622eb
LordUbuntu <therealjacoburger@gmail.com>
2023-08-18 17:34:28 -0700

    added redden method

--- color_filter.py
+++ color_filter.py
@@ -52,3 +52,12 @@ def mutate(image_path: str, output_path = "output.jpg", color = (0, 0, 0)):
     image.save(output_path)
 
 
+def redden(image_path: str, output_path = "output.jpg"):
+    image = Image.open(image_path)
+
+    for pixel in product(range(image.width), range(image.height)):
+        old_color = image.getpixel(pixel)
+        new_color = (max(old_color[1], old_color[2]), 0, 0)
+        image.putpixel(pixel, new_color)
+
+    image.save(output_path)
