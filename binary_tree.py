078f37e918816110ace5ee92993217fae18cb393
Jacobus Burger <therealjacoburger@gmail.com>
2023-09-20 16:42:59 -0700

    feature: BTree insert

--- binary_tree.py
+++ binary_tree.py
@@ -4,8 +4,22 @@
 
 
 # recursive node based tree
-class BinaryNodeTree:
+class BTree:
     def __init__(self, data=None, left=None, right=None):
         self.data = data
         self.left = left
         self.right = right
+
+    def insert(self, data):
+        if data >= self.data:
+            if self.right is None:
+                self.right = BTree(data)
+            else:
+                self.right.insert(data)
+        if data < self.data:
+            if self.left is None:
+                self.left = BTree(data)
+            else:
+                self.left.insert(data)
+        else:
+            self.data = data
