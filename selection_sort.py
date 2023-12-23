06661c095d212dd125486fc4d91f9569bf0c7d37
Jacobus Burger <therealjacoburger@gmail.com>
2023-12-23 15:06:30 -0800

    added seleciton sort

---
+++ selection_sort.py
@@ -0,0 +1,13 @@
+# Jacobus Burger (2023)
+# A simple selection sort algorithm
+
+def sort(l: list) -> list:
+    for current in range(len(l)):
+        min = i  # assume min is first element
+        # search for a smaller value in the indices after current
+        for index in range(current + 1, len(l)):
+            if l[index] < l[min]:
+                min = index
+        # swap elements if min is a different greater index
+        l[current], l[min] = l[min], l[current]
+    return l
