1f12e22be8a4a9c1361873c4821e910bc7fb769a
Jacobus Burger <therealjacoburger@gmail.com>
2023-12-24 15:05:36 -0800

    added in-place sorting instead of copy sorting

--- bubble_sort.py
+++ bubble_sort.py
@@ -3,13 +3,12 @@
 # Bubble sort compaires pairs of adjacent values and swaps them
 # if they're out of order. It's worst case is O(n^2), not great.
 def sort(l: list) -> list:
-    result = l.copy()
-    for _ in range(1, len(result)):
+    for _ in range(1, len(l)):
         sorted = True
-        for index in range(1, len(result)):
-            if result[index - 1] >= result[index]:
+        for index in range(1, len(l)):
+            if l[index - 1] >= l[index]:
                 sorted = False
-                result[index - 1], result[index] = result[index], result[index - 1]
+                l[index - 1], l[index] = l[index], l[index - 1]
         if sorted == True:
             break
-    return result
+    return l
