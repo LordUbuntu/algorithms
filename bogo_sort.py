b9a0221c2476974d2cb6c45b882a306ed95170c5
Jacobus Burger <therealjacoburger@gmail.com>
2023-12-26 17:27:28 -0800

    added sort rate and corrected typo

--- bogo_sort.py
+++ bogo_sort.py
@@ -3,15 +3,16 @@ from random import sample
 from time import sleep
 
 
-def bogo(arr: list) -> list:
+def bogo(arr: list, sort_per_second: int = 1) -> list:
     while arr != sorted(arr):
-        i = randint(0, len(list) - 1)
-        j = randint(0, len(list) - 1)
+        i = randint(0, len(arr) - 1)
+        j = randint(0, len(arr) - 1)
         arr[i], arr[j] = arr[j], arr[i]
         print(arr)
-        sleep(1)
+        sleep(1 / sort_per_second)
     return arr
 
 
 if __name__ == "__main__":
-    bogo(sample(range(0, 100), randint(1, 30)))
+    sorts_per_second = int(input("how many sorts each second?"))
+    bogo(sample(range(0, 100), randint(1, 30)), sorts_per_second)
