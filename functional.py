8bbc8a2fce69fd98b324da59f5d3c0ecfe9de07d
Jacobus Burger <therealjacoburger@gmail.com>
2024-06-14 13:02:59 -0700

    added filter, map, reduce, and apply

--- functional.py
+++ functional.py
@@ -2,6 +2,14 @@
 # Functional Algorithms and Data Structures
 
 
+def filter(f, arr):
+    result = []
+    for val in arr:
+        if f(val):
+            result.append(val)
+    return result
+
+
 def map(f, arr):
     result = []
     for val in arr:
@@ -13,4 +21,11 @@ def reduce(f, arr, init=None):
     result = 0 if init is None else init
     for val in arr:
         result = f(result, val)
+    return result
+
+
+def apply(f, arr):
+    result = []
+    for val in arr:
+        result.append(f(val))
     return result
