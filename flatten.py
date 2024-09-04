3327a714d50259740e65ec105f797172b1cc0b77
Jacobus Burger <therealjacoburger@gmail.com>
2024-09-03 20:29:26 -0700

    added nieve flatten algorithm

---
+++ flatten.py
@@ -0,0 +1,7 @@
+# Jacobus Burger (2024-09-03)
+# Flattens a higher dimensional data structure into one dimension
+
+
+# nieve solution assuming a generator (Python specific)
+def flatten(generator):
+    return [*generator]
