645c63030f48a561fa352fbc2ca33a2be5b15101
Jacobus Burger <therealjacoburger@gmail.com>
2024-10-15 22:35:26 -0700

    added to comment

--- Sorting/bubble_sort.py
+++ Sorting/bubble_sort.py
@@ -2,7 +2,7 @@
 # Bubble Sort
 # Bubble sort compaires pairs of adjacent values and swaps them
 #     if they're out of order. It's worst case is O(n^2). So not
-#     great on big inputs...
+#     great on big inputs but fine for smaller ones
 def sort(l: list) -> list:
     for _ in range(1, len(l)):
         sorted = True
