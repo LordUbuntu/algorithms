31f720363ea270f99ee8dd26b3d559eab41b86e0
Jacobus Burger <therealjacoburger@gmail.com>
2024-10-17 21:20:23 -0700

    add: adjacency_list

---
+++ Graph/adjacency_list.py
@@ -0,0 +1,7 @@
+# Jacobus Burger (Oct 2024)
+# Data Structure:
+#   Adjacency List
+# Desc:
+#   An implementation of a generalized graph structure
+# Info:
+#   https://en.wikipedia.org/wiki/Adjacency_list
