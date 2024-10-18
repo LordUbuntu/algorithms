343fc3ac12a3f95d91a310e7373fe85615356937
Jacobus Burger <therealjacoburger@gmail.com>
2024-10-17 21:58:02 -0700

    init: start plan to include quantum algorithms, starting with deutsch-jozsa algorithm

---
+++ Quantum/deutsch_jozsa.py
@@ -0,0 +1,7 @@
+# Jacobus Burger (TODO)
+# Algorithm:
+#   Deutsch-Jozsa Algorithm
+# Desc:
+#   A fantastically useless algorithm to demonstrate quantum computing
+# Info:
+#   https://en.wikipedia.org/wiki/Deutsch%E2%80%93Jozsa_algorithm
