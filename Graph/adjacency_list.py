8fac68efa3c007ebaccd18654afc7ad4c2e0f2b6
Jacobus Burger <therealjacoburger@gmail.com>
2024-10-17 21:25:38 -0700

    add: basic example

--- Graph/adjacency_list.py
+++ Graph/adjacency_list.py
@@ -5,3 +5,19 @@
 #   An implementation of a generalized graph structure
 # Info:
 #   https://en.wikipedia.org/wiki/Adjacency_list
+
+# This is a simple diamond graph example using Guido van Rossum's
+#   hash table implementation approach (using a dictionary)
+#
+#    B
+#  /   \
+# A     D
+#  \   /
+#    C
+#
+G = {
+    'A': ['B', 'C'],
+    'B': ['A', 'D'],
+    'C': ['A', 'D'],
+    'D': ['B', 'C']
+}
