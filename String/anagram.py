497d008f792cc2d62e7841269b1a75985114bb97
Jacobus Burger <therealjacoburger@gmail.com>
2024-10-21 19:37:50 -0700

    added info for anagram.py

--- String/anagram.py
+++ String/anagram.py
@@ -1,4 +1,11 @@
-# rearragements of a word
+# Jacobus Burger (Oct 2024)
+# Algorithm:
+#   Anagram
+# Desc:
+#   An algorithm that determines if two strings are
+#   rearrangements of each other
+# Info:
+#   https://en.wikipedia.org/wiki/Anagram
 def anagram(a, b):
     if sorted(a) == sorted(b):
         return True
