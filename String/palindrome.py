63f886699525319d673022df990d7bb2e010be70
Jacobus Burger <therealjacoburger@gmail.com>
2024-10-21 19:40:34 -0700

    added info

--- String/palindrome.py
+++ String/palindrome.py
@@ -1,4 +1,10 @@
-# reversal of a word
+# Jacobus Burger (Oct 2024)
+# Algorithm:
+#   Palindrome
+# Desc:
+#   An algorithm that sees if two strings are reversals of eachother
+# Info:
+#   https://en.wikipedia.org/wiki/Palindrome
 def palindrome(a, b):
     if a == reversed(b):
         return True
