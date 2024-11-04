301d0bf40c3c1c63bdbcb39175c8040522cbe5ef
Jacobus Burger <therealjacoburger@gmail.com>
2024-11-04 12:40:44 -0800

    added docstring to clarify

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -5,5 +5,28 @@
 #   during the blackout today.
 
 
-def sort(A: list) -> list:
-    pass
+def sort(A: list[int | float]) -> list[int | float]:
+    """
+    sort(A: list) -> list
+
+    Sorts an array `A` using selection sort according to the implementation
+        specified in "Introduction to Algorithms 3rd Ed."
+
+    The following pseudocode is how the algorithm should work (pg 18)
+
+    ```
+    Input: A sequence of n numbers <a1, a2, ..., an>.
+    Output: A permutation (reordering) <a1', a2', ..., an'> of the input
+        sequence such that a1' <= a2' <= ... <= an'.
+
+    for j = 2 to A.length
+        key = A[j]
+        // Insert A[j] into the sorted sequence A[1..j - 1]
+        i = j - 1
+        while i > 0 and A[i] > key
+            A[i + 1] = A[i]
+            i = i - 1
+        A[i + 1] = key
+    ```
+
+    """
