f71f69b074d7b0e776ba027a875cd80244955b3b
Jacobus Burger <therealjacoburger@gmail.com>
2024-11-04 12:49:34 -0800

    selection sort implemented

--- Sorting/selection_sort.py
+++ Sorting/selection_sort.py
@@ -32,4 +32,7 @@ def sort(A: list[int | float]) -> list[int | float]:
     for j in range(1, len(A) - 1):
         key = A[j]
         i = j - 1
-        pass
+        while i > 0 and A[i] > key:
+            A[i + 1] = A[i]
+            i = i - 1
+        A[i + 1] = key
