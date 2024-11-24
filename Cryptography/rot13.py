51200aa6cbed7e7f1d121f3d2654cfbed4dd41d2
Jacobus Burger <therealjacoburger@gmail.com>
2024-11-23 17:04:20 -0800

    added rot13 to algorithms

---
+++ Cryptography/rot13.py
@@ -0,0 +1,3 @@
+# Jacobus Burger (2024-11-23)
+# ROT13, ROTate 13 Cypher
+# A simple rotational cypher algorithm that rotates the values of symbols (letters) within a symbol space (alphabet) over by 13 symbols within that order. Imagine arranging the alphabet in a ring, then take any letter and move 13 in a chosen direction. To decypher, rotate all the letters back the other way.
