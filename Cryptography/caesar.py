009422cdcb8133ded333382dc11e008b5d1042ef
Jacobus Burger <therealjacoburger@gmail.com>
2024-11-28 22:51:22 -0800

    new algo added

---
+++ Cryptography/caesar.py
@@ -0,0 +1,8 @@
+# Jacobus Burger (2024-11-30)
+# Algorithm:
+#   Caesar Cypher
+# Desc:
+#   A substitution cypher supposedly used by the roman general and
+#   dictator Gaius Julius Caesar
+# Info:
+#   https://en.wikipedia.org/wiki/Caesar_cipher
