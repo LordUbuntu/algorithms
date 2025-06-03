7bc670eb90bd030cde11652e0b13a37988b06da8
Jacobus Burger <therealjacoburger@gmail.com>
2025-06-02 17:20:45 -0700

    add: generic void* alternative of binary search in C99

--- Binary Search/binary_search.c
+++ Binary Search/binary_search.c
@@ -2,9 +2,9 @@
 // binary search on an array of values, O(log n) time
 // see: https://en.wikipedia.org/wiki/Binary_search_algorithm
 #include <stdio.h>
+#include <stddef.h>
 
 
-// TODO: live life dangerously and generically with void*
 int search(int target, int *array, int length) {
         int left = 0, right = length - 1;
         while (left <= right) {
@@ -19,6 +19,33 @@ int search(int target, int *array, int length) {
         }
         return -1;
 }
+
+
+// living dangerously with a generic version
+int binary_search(
+        void *array,
+        size_t length,
+        size_t element_size,
+        void *target,
+        int (*cmp)(void* a, void* b)
+) {
+        size_t left = 0;
+        size_t right = length - 1;
+        while (left <= right) {
+                size_t middle = (left + right) / 2;
+                void *current = array + (element_size * middle);
+                int diff = cmp(target, current);
+                if (diff == 0) {
+                        return middle;
+                } else if (diff > 0) {
+                        left = middle + 1;
+                } else if (diff < 0) {
+                        right = middle - 1;
+                }
+        }
+        return -1;
+}
+
 
 
 int main(void) {
