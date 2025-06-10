130b859b6481ba7ede78dba8902a2caeb7900a71
Jacobus Burger <therealjacoburger@gmail.com>
2025-06-09 21:15:09 -0700

    add string interleaving

---
+++ Interleave/interleave_string.py
@@ -0,0 +1,21 @@
+from sys import argv
+
+
+def interleave(a: str, b: str) -> str:
+    """
+    Interleave 2 strings together.
+
+    1. zip elements together pair-wise
+    2. concatenate pairs
+    3. return full string
+    """
+    return ''.join([a + b for a, b in zip(a, b)])
+
+
+if __name__ == "__main__":
+    if len(argv) < 3:
+        quit()
+    a = argv[1]
+    b = argv[2]
+    c = interleave(a, b)
+    print("{}\n{}\n{}\n\n".format(a, b, c))
