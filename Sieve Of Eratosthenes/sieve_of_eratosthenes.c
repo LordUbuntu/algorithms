1ba08af906264e5eba461a13d4348cec0861f13e
Jacobus Burger <therealjacoburger@gmail.com>
2025-06-14 19:21:59 -0700

    initial implementation (some bugs)

--- Sieve Of Eratosthenes/sieve_of_eratosthenes.c
+++ Sieve Of Eratosthenes/sieve_of_eratosthenes.c
@@ -1,18 +1,27 @@
 /* Jacobus Burger (2025)
  * Sieve of Eratosthenes in C
  * see: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+ *
+ * note: when compiling be sure to link math library. eg: -lm
  */
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <math.h>
 
 
 bool *sieve(int n) {
-        // initialize array to all true except for index 0 (the number 1)
+        // initialize array to all true except for index/numbers 0 and 1
         bool *primes = (bool*) calloc(n, sizeof(bool));
-        memset(primes + 1, 1, n * sizeof(bool));
+        memset(primes + 2, 1, n * sizeof(bool));
 
         // run sieve of eratosthenes
+        for (int p = 2; p < sqrt(n) + 1; p++)
+                if (primes[p])
+                        for (int i = p^2; i < n + 1; i += p)
+                                primes[i] = 0;
+
+        // return solution
         return primes;
 }
 
@@ -27,12 +36,11 @@ int main(int argc, char *argv[]) {
         // print result
         printf("%i\n", n);
         for (int i = 0; i < n; i++) {
-                printf("%i ", primes[i]);
-                // // print array elements that are prime
-                // if (primes[i])
-                //         printf("%i ", i + 1);
-                // else
-                //         continue;
+                // print array elements that are prime
+                if (primes[i])
+                        printf("%i ", i);
+                else
+                        continue;
         }
         printf("\n\n");
         // end program
