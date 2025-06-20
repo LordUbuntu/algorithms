a84214bf2b401d4e90b92e4c05ca00c9ede92742
Jacobus Burger <therealjacoburger@gmail.com>
2025-06-20 15:44:33 -0700

    simplified Python implementation

--- Sieve Of Eratosthenes/sieve_of_eratosthenes.py
+++ Sieve Of Eratosthenes/sieve_of_eratosthenes.py
@@ -7,24 +7,21 @@ from math import sqrt as sqrt
 
 def sieve(n: int):
     # initialize all except 0 and 1 to True
-    sieve = [True if i > 1 else False for i in range(n + 1)]
-    # iterate through all integers from 2 to sqrt(n)
+    primes = [True if i > 1 else False for i in range(n + 1)] # iterate through all integers from 2 to sqrt(n)
     for p in range(2, int(sqrt(n)) + 1):
         # skip any non-primes
-        if sieve[p] == True:
+        if primes[p] == True:
             # eliminate all multiples i of current prime p, from p^2 to n
             for i in range(p**2, n + 1, p):
-                sieve[i] = False
+                primes[i] = False
     # you now have the sieve of eratosthenes
-    return sieve
+    return primes
 
 
 if __name__ == "__main__":
     # get n
     n = int(argv[1])
-    # sieve primes up to n
-    prime_sieve = sieve(n)
     # get list of prime numbers using sieve and list comprehension
-    primes = " ".join([str(i) for i in range(len(prime_sieve)) if prime_sieve[i]])
+    primes = " ".join([str(i) for i in range(len(sieve(n))) if prime_sieve[i]])
     # print result
     print("{}\n{}\n\n".format(n, primes))
