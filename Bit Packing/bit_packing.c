996d62e15c4c20cf1d2c1eab9491ce279162f493
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-03 15:55:21 -0700

    finish main function

--- Bit Packing/bit_packing.c
+++ Bit Packing/bit_packing.c
@@ -1,8 +1,5 @@
 // Jacobus Burger (2025-07-03)
-// Implementation of bit packing technique. Just single bits like a
-//      packed array of booleans is demonstrated here, but with a simple
-//      change it can be extended for a packed segment of any size
-//      smaller than the data it is being packed into
+// Implementation of bit packing technique.
 // Related concepts seem to be bit arrays which I leive I've implemented
 //      here, and bit fields which are a bit different but still related.
 // See:
@@ -10,4 +7,20 @@
 // - https://www.cs.emory.edu/%7Echeung/Courses/255/Syllabus/1-C-intro/bit-array.html
 // - https://en.wikipedia.org/wiki/Bit_array
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdint.h>
+
+
+int main(void) {
+        // initialize pack to all 0
+        uint8_t pack = 0;
+        printf("%032b\n", pack);
+        // set packed bits
+        set_bit(pack, n);
+        printf("%032b\n", pack);
+        // get packed bits
+        printf("%032b %032b\n", pack, get_bit(pack, n));
+        // clear packed bits
+        clear_bit(pack, n);
+        printf("%032b\n", pack);
+        return 0;
+}
