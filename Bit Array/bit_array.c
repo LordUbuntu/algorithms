e9237ccaad61bd45ae440a2afeb27c64974cc7a8
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-03 18:30:09 -0700

    implement bit array (C)

--- Bit Array/bit_array.c
+++ Bit Array/bit_array.c
@@ -9,3 +9,52 @@
 //      address in array. Then the emory info can be used to produce
 //      macros for setting and reading bits along an array
 //      This can be used in the sieve_of_eratosthenes.c file!
+#include <limits.h>
+#include <stdio.h>
+#include <stddef.h>
+
+
+#define BITS(DATA) (sizeof(DATA) * CHAR_BIT)
+#define SET(A, i) (A[(i / BITS(A[0]))] |= (1 << (i % BITS(A[0]))))
+#define GET(A, i) (A[(i / BITS(A[0]))] & (1 << (i % BITS(A[0]))))
+#define TOGGLE(A, i) (A[(i / BITS(A[0]))] ^= (1 << (i % BITS(A[0]))))
+#define CLEAR(A, i) (A[(i / BITS(A[0]))] &= ~(1 << (i % BITS(A[0]))))
+
+
+int main(int argc, char *argv[]) {
+        int A[3];
+
+        // show initial state
+        printf("INITIAL STATE\n");
+        printf("bits in int: %i\n", BITS(A[0]));
+        for (size_t i = 0; i < 3; i++)
+                printf("A[%i] = %032b\n", i, A[i]);
+        printf("\n");
+
+        // set bits
+        SET(A, 30);
+        SET(A, 50);
+        SET(A, 70);
+
+        // show change in state
+        for (size_t i = 0; i < 3; i++)
+                printf("A[%i] = %032b\n", i, A[i]);
+        printf("\n");
+
+        // test get
+        for (size_t i = 0; i < 3 * BITS(A[0]); i++)
+                if (GET(A, i))
+                        printf("bit %i is set\n", i);
+
+        // toggle bit
+        printf("state before toggle: %032b\n", A[0]);
+        TOGGLE(A, 7);
+        printf("state after toggle: %032b\n", A[0]);
+
+        // test clear
+        printf("state before clear: %032b\n", A[0]);
+        CLEAR(A, 30);
+        if (~GET(A, 30))
+                printf("unset successful\n");
+        printf("state before clear: %032b\n", A[0]);
+}
