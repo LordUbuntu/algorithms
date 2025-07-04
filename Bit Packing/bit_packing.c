64dc9db965e5c76181235395c9133dbd4279d0fc
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-03 17:23:59 -0700

    implemen bit array packing and unpacking macros

--- Bit Packing/bit_packing.c
+++ Bit Packing/bit_packing.c
@@ -2,25 +2,39 @@
 // Implementation of bit packing technique.
 // Related concepts seem to be bit arrays which I leive I've implemented
 //      here, and bit fields which are a bit different but still related.
+// Note: is should be weary of endianness.
+// note: when compiling be sure to link math library. eg:
+//      gcc bit_packing.c -lm -o bit_packing
 // See:
 // - https://www.cs.cornell.edu/courses/cs3410/2024fa/notes/bitpack.html
 // - https://www.cs.emory.edu/%7Echeung/Courses/255/Syllabus/1-C-intro/bit-array.html
 // - https://en.wikipedia.org/wiki/Bit_array
+#include <math.h>
 #include <stdio.h>
 #include <stdint.h>
 
 
+// BA = Bit Array
+// D = Data
+// O = Offset
+#define PACK(BA, D, O)  (BA = BA | (D << O))
+#define READ(BA, O)     ((BA & ((size_t)(pow(2, O) - 1) << O)) >> O)
+#define CLEAR(BA, O)    (BA = BA & ~((size_t)(pow(2, O) - 1) << O))
+
+
 int main(void) {
         // initialize pack to all 0
-        uint8_t pack = 0;
-        printf("%032b\n", pack);
+        uint16_t pack = 0;
+        uint8_t data = 0b1101;
+        size_t n = 4;
+        printf("%016b %08b %i\n", pack, data, n);
         // set packed bits
-        set_bit(pack, n);
-        printf("%032b\n", pack);
+        PACK(pack, data, n);
+        printf("%016b %08b %i\n", pack, data, n);
         // get packed bits
-        printf("%032b %032b\n", pack, get_bit(pack, n));
+        printf("%016b %08b %i %016b\n", pack, data, n, READ(pack, n));
         // clear packed bits
-        clear_bit(pack, n);
-        printf("%032b\n", pack);
+        CLEAR(pack, n);
+        printf("%016b %08b %i\n", pack, data, n);
         return 0;
 }
