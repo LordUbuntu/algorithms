627431809b8678fa880e08f67b60008a34cba561
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-05 17:45:20 -0700

    implement bit field (C)

---
+++ Bit Field/bit_field.c
@@ -0,0 +1,27 @@
+/* Jacobus Burger (2025-07-05)
+ * Implementation of a bit field (a compact represenation of data)
+ * See:
+ * - https://en.wikipedia.org/wiki/Bit_field
+ * - https://www.gnu.org/software/c-intro-and-ref/manual/html_node/Bit-Field-Packing.html
+ */
+#include <stdio.h>
+#include <stdlib.h>
+
+
+#define A 0b01100001
+#define B 0b01100010
+#define C 0b01100011
+
+
+int main(int argc, char *argv[]) {
+        // get input
+        if (argc < 2)
+                return 1;
+        char letter = argv[1][0];
+
+        // show letter selected
+        printf("%c %08b\n", letter, letter);
+        printf("%08b %08b %s\n", letter, A, A == letter ? "match" : "");
+        printf("%08b %08b %s\n", letter, B, B == letter ? "match" : "");
+        printf("%08b %08b %s\n", letter, C, C == letter ? "match" : "");
+}
