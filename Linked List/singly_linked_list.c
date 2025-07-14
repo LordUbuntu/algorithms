8fd16dd60df3783c9c73f12bb4a1e369221d992a
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-13 23:01:07 -0700

    i really need to figure out what is going on here

--- Linked List/singly_linked_list.c
+++ Linked List/singly_linked_list.c
@@ -16,13 +16,22 @@ int main(int argc, char *argv[]) {
                 return 1;
         int n = atoi(argv[1]);
 
+        printf("n: %i\n", n);
+
+        int val = 1;
+        if (argc > 2)
+                val = atoi(argv[2]);
+
         struct Node *list;
-        struct Node **head = &list;
-        struct Node **tail = head;
-        for (int i = 2; i < MAX(argc, n); i++) {
-                (**tail).value = atoi(argv[i]);
-                (**tail).next = &(struct Node){.value = 0, .next = NULL};
-                printf("%i, %i\n", (**tail).value, (**tail).next);
-                *tail = (**tail).next;
-        }
+        struct Node *a;
+        struct Node *b;
+
+        list->value = val;
+        list->next = a;
+        list->next->value = 11;
+        list->next->next = b;
+        list->next->value = 13;
+
+        printf("%i %i %i\n", list->value, list->next->value, list->next->next->value);
+
 }
