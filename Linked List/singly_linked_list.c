07b2c6a1b3e3222f71f5eee9eca3fd6900e6933b
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-14 22:58:48 -0700

    try with simpler syntax i like

--- Linked List/singly_linked_list.c
+++ Linked List/singly_linked_list.c
@@ -5,30 +5,20 @@
 #define MAX(A, B) (A > B ? A : B)
 
 
-struct Node {
+struct node {
         int value;
-        struct Node *next;
-};
+        struct node* next;
+} node_t;
 
 
-struct Node *new_node(int value) {
-        struct Node *node = (struct Node*) malloc(sizeof(struct Node));
-        node->value = value;
-        node->next = NULL;
-        return node;
-}
-
-
-void delete_node(struct Node *node) {
-        free(node);
-}
-
-
-void append_node(struct Node *head, struct Node *node) {
-        struct Node *current = head;
+void append(node_t* head, int value) {
+        node_t* current = head;
         while (current->next != NULL)
                 current = current->next;
-        current->next = node;
+
+        current->next = (node_t*) malloc(sizeof(node_t));
+        current->next->value = value;
+        current->next->next = NULL;
 }
 
 
@@ -38,18 +28,4 @@ int main(int argc, char *argv[]) {
         int n = atoi(argv[1]);
 
         printf("n: %i\n", n);
-
-        struct Node *head;
-        struct Node *a = new_node(13);
-        struct Node *b = new_node(31);
-        struct Node *c = new_node(131);
-        head = a;
-        append_node(head, b);
-        append_node(head, c);
-        printf("%i: %i %i %i\n", &a, a->value, a->next, a->next ? a->next->value : 0);
-        printf("%i: %i %i %i\n", &b, b->value, b->next, b->next ? b->next->value : 0);
-        printf("%i: %i %i %i\n", &c, c->value, c->next, c->next ? c->next->value : 0);
-        delete_node(a);
-        delete_node(b);
-        delete_node(c);
 }
