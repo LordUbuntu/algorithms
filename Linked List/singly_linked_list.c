e6f98ff78717d7d43c57aa3fa4884c6cf073ad3c
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-14 22:50:23 -0700

    add starting functions

--- Linked List/singly_linked_list.c
+++ Linked List/singly_linked_list.c
@@ -11,6 +11,27 @@ struct Node {
 };
 
 
+struct Node *new_node(int value) {
+        struct Node *node = (struct Node*) malloc(sizeof(struct Node));
+        node->value = value;
+        node->next = NULL;
+        return node;
+}
+
+
+void delete_node(struct Node *node) {
+        free(node);
+}
+
+
+void append_node(struct Node *head, struct Node *node) {
+        struct Node *current = head;
+        while (current->next != NULL)
+                current = current->next;
+        current->next = node;
+}
+
+
 int main(int argc, char *argv[]) {
         if (argc < 2)
                 return 1;
@@ -18,20 +39,17 @@ int main(int argc, char *argv[]) {
 
         printf("n: %i\n", n);
 
-        int val = 1;
-        if (argc > 2)
-                val = atoi(argv[2]);
-
-        struct Node *list;
-        struct Node *a;
-        struct Node *b;
-
-        list->value = val;
-        list->next = a;
-        list->next->value = 11;
-        list->next->next = b;
-        list->next->value = 13;
-
-        printf("%i %i %i\n", list->value, list->next->value, list->next->next->value);
-
+        struct Node *head;
+        struct Node *a = new_node(13);
+        struct Node *b = new_node(31);
+        struct Node *c = new_node(131);
+        head = a;
+        append_node(head, b);
+        append_node(head, c);
+        printf("%i: %i %i %i\n", &a, a->value, a->next, a->next ? a->next->value : 0);
+        printf("%i: %i %i %i\n", &b, b->value, b->next, b->next ? b->next->value : 0);
+        printf("%i: %i %i %i\n", &c, c->value, c->next, c->next ? c->next->value : 0);
+        delete_node(a);
+        delete_node(b);
+        delete_node(c);
 }
