37c8506713999a87583a2609dea2c788a3ad222f
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-16 21:19:06 -0700

    add basic functions

--- Linked List/singly_linked_list.c
+++ Linked List/singly_linked_list.c
@@ -3,9 +3,6 @@
 #include <stdio.h>
 
 
-#define MAX(A, B) (A > B ? A : B)
-
-
 typedef struct node {
         int value;
         struct node* next;
@@ -35,22 +32,22 @@ void append(node_t** head, int value) {
 }
 
 
-int truncate(node_t* head) {
-        // remove from empty list
-        if (head == NULL) {
-                return INT_MAX;  // outside usual possibilities
+int truncate(node_t** head) {
+        // remove from empty list (return error value INT_MIN)
+        if (*head == NULL) {
+                return INT_MIN;
         }
 
         // remove from list with one element (decapitate)
-        if (head->next == NULL) {
-                int value = head->value;
-                free(head);
-                head = NULL;
+        if ((*head)->next == NULL) {
+                int value = (*head)->value;
+                free(*head);
+                *head = NULL;
                 return value;
         }
 
         // remove from list with more than one element (amputate)
-        node_t* current = head;
+        node_t* current = *head;
         while (current->next->next != NULL)
                 current = current->next;
         int value = current->next->value;
@@ -63,4 +60,8 @@ int truncate(node_t* head) {
 int main(int argc, char *argv[]) {
         node_t* head = NULL;
         append(&head, 1);
+        append(&head, 2);
+        printf("%i\n", truncate(&head));
+        printf("%i\n", truncate(&head));
+        printf("%i\n", truncate(&head));
 }
