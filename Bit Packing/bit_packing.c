b8d0ed174ba6716763d1044a7d7e6864a8c9d4fa
Jacobus Burger <therealjacoburger@gmail.com>
2025-07-22 20:18:03 -0700

    could a mask be implemented without pow?

--- Bit Packing/bit_packing.c
+++ Bit Packing/bit_packing.c
@@ -17,6 +17,7 @@
 // READ unpacks N bits from bit pack P at offset O
 // CLEAR zeroes out N bits from bit pack P at offset O
 #define MASK(N)         ( ((size_t)pow(2, N)) - 1 )
+//      ^ maybe there's a way to implement a mask without relying on pow?
 #define PACK(P, D, O)   ( P |= (D << O) )
 #define READ(P, N, O)   ( (P & (MASK(N) << O)) >> O )
 #define CLEAR(P, N, O)  ( P &= ~(MASK(N) << O) )
