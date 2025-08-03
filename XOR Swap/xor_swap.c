10f23fbe9b1dd87642020729f57dc2b7c982eb84
Jacobus Burger <therealjacoburger@gmail.com>
2025-08-02 20:34:18 -0700

    XOR Swap (C99)

---
+++ XOR Swap/xor_swap.c
@@ -0,0 +1,19 @@
+/* Jacobus Burger (2025-08-02)
+ * XOR Swap (C99)
+ * description:
+ * A common method of swapping values is to use a temporary variable, but
+ *      it is possible to swap two variables without the use of a
+ *      temporary variable by utilizing a sequence of 3 xor operations.
+ * see:
+ * - https://en.wikipedia.org/wiki/XOR_swap_algorithm
+ */
+
+
+void swap(void* a, void* b) {
+        // XOR swap when a == b sets to 0, so avoid it
+        if (a == b) return;
+        // XOR swap a and b
+        *a ^= *b; // a = b xor a
+        *b ^= *a; // b = a xor b
+        *a ^= *b; // a = b xor a
+}
