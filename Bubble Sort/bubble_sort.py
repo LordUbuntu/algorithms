73f26934b53c0ddf9d5b5a4b94a29003fc1d5dab
Jacobus Burger <therealjacoburger@gmail.com>
2025-08-02 19:12:53 -0700

    create folder for bubble sort

--- Bubble Sort/bubble_sort.py
+++ Bubble Sort/bubble_sort.py
@@ -1,12 +1,10 @@
 # Jacobus Burger (2022)
 # Bubble Sort
-# Bubble sort compaires pairs of adjacent values and swaps them
-#     if they're out of order. It's worst case is O(n^2). So not
-#     great on big inputs...
+# see:
+# - https://en.wikipedia.org/wiki/Bubble_sort
 def sort(array: list) -> list:
     for _ in range(1, len(array)):
         for index in range(1, len(array)):
             if array[index - 1] >= array[index]:
-                sorted = False
                 array[index - 1], array[index] = array[index], array[index - 1]
     return array
