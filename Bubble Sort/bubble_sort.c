ae8877b14ba2b89c1836b5977efc5b56abaa2f4e
Jacobus Burger <therealjacoburger@gmail.com>
2025-08-11 12:02:57 -0700

    align to K&R

--- Bubble Sort/bubble_sort.c
+++ Bubble Sort/bubble_sort.c
@@ -6,15 +6,18 @@
 #include <stdio.h>
 
 
-int* sort(int* array, int length) {
-        for (int i = 1; i < length; i++)
-                for (int j = 1; j < length; j++)
+// might be possible to do this with void *array instead
+void sort(int *array, int length) {
+        for (int i = 1; i < length; i++) {
+                for (int j = 1; j < length; j++) {
                         if (array[j - 1] >= array[j]) {
                                 // XOR swap
                                 array[j - 1] ^= array[j];
                                 array[j] ^= array[j - 1];
                                 array[j - 1] ^= array[j];
                         }
+                }
+        }
 }
 
 
@@ -24,16 +27,18 @@ int main(void) {
         int array[LEN] = {2, 1, 3, 7, 4, 5, 9, 8, 0, 6}
 
         // show unsorted array
-        for (int i = 0; i < LEN; i++)
+        for (int i = 0; i < LEN; i++) {
                 printf("%i ", array[i]);
+        }
         puts("");
 
         // sort array
         sort(array, LEN);
 
         // show sorted array
-        for (int i = 0; i < LEN; i++)
+        for (int i = 0; i < LEN; i++) {
                 printf("%i ", array[i]);
+        }
         puts("");
 
         // end
