72cec41ff50ae992bbdabbd210d1534d3ca9c038
Jacobus Burger <therealjacoburger@gmail.com>
2025-08-18 16:05:58 -0700

    Dynamic Array (C99)

---
+++ Stack/stack.py
@@ -0,0 +1,43 @@
+# Jacobus Burger (2025-08-18)
+# Stack (Python 3)
+# Description:
+# Stacks are a First In Last Out (FILO) data structure where you can
+#      `push` items to the top, or `pop` items from the top, like adding
+#      and removing sheets of paper from the top of a stack of paper.
+# Info:
+# - https://en.wikipedia.org/wiki/Stack_(abstract_data_type)
+
+
+# Stack Abstract Data Type (ADT)
+# head: top of stack index (and size of stack)
+# data: array of data (static data for now)
+class Stack:
+    def __init__(self, head: int = 0, data: list = []) -> None:
+        self.head = head
+        self.data = data
+
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
+    def push(self, value: int) -> None:
+        self.data.append(value)
+        self.head += 1
+
+    # Time Complexity: O(1)
+    # Space Complexity: O(1)
+    def pop(self) -> int:
+        if self.head <= 0:
+            return -1
+        value = self.data.pop()
+        self.head -= 1
+        return value
+
+
+if __name__ == "__main__":
+    s = Stack()
+    s.push(1)
+    s.push(2)
+    s.push(3)
+    print(s.pop())
+    print(s.pop())
+    print(s.pop())
+    print(s.pop())
