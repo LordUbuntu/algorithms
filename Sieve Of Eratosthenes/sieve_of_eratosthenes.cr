251872c8a1607d34916d2298abaadf0233d57c68
Jacobus Burger <therealjacoburger@gmail.com>
2025-08-19 19:19:25 -0700

    Update documentation and minor refactor

--- Sieve Of Eratosthenes/sieve_of_eratosthenes.cr
+++ Sieve Of Eratosthenes/sieve_of_eratosthenes.cr
@@ -1,8 +1,20 @@
 # Jacobus Burger (2025-06-20)
-# Sieve of Eratosthenes in Crystal
-# see: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+# Sieve of Eratosthenes (Crystal 1.17)
+# The Sieve of Eratosthenes is an ancient and effective algorithm for finding
+#      all the prime numbers up to a given limit through a process of
+#      elimination (like how a sieve filters out bigger particles). It does
+#      this by iteratively canceling out all the multiples of numbers starting
+#      from the first known prime number 2, thus canceling out every even
+#      number besides 2, then it starts with the next number which is then
+#      known to be prime too, and it keeps iterating on this process until all
+#      composite numbers are eliminated. Once all known composites are
+#      removed, what remains are all the prime numbers in the range!
+# Info:
+# - https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 
 
+# Time Complexity: O(n log log n)
+# Space Complexity: O(n)
 def sieve(n : Int64)
   primes : Array(Bool) = Array.new(n + 1) { |i| i < 2 ? false : true }
 
@@ -18,8 +30,11 @@ end
 
 
 def main
+  # get n
   n = gets.try(&.to_i) || 0
+  # get list of primes
   primes = sieve(n)
+  # print result
   puts n
   n.times do |i|
     if primes[i]
