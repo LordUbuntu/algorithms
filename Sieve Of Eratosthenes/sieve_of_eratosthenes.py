956160c6c478238416a76e03ff62c27c288d7658
Jacobus Burger <therealjacoburger@gmail.com>
2025-08-19 19:19:29 -0700

    Update documentation and minor refactor

--- Sieve Of Eratosthenes/sieve_of_eratosthenes.py
+++ Sieve Of Eratosthenes/sieve_of_eratosthenes.py
@@ -1,10 +1,22 @@
 # Jacobus Burger (2022)
-# Sieve of Eratosthenes in Python
-# see: https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
+# Sieve of Eratosthenes (Python 3)
+# The Sieve of Eratosthenes is an ancient and effective algorithm for finding
+#      all the prime numbers up to a given limit through a process of
+#      elimination (like how a sieve filters out bigger particles). It does
+#      this by iteratively canceling out all the multiples of numbers starting
+#      from the first known prime number 2, thus canceling out every even
+#      number besides 2, then it starts with the next number which is then
+#      known to be prime too, and it keeps iterating on this process until all
+#      composite numbers are eliminated. Once all known composites are
+#      removed, what remains are all the prime numbers in the range!
+# Info:
+# - https://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
 from sys import argv
 from math import sqrt as sqrt
 
 
+# Time Complexity: O(n log log n)
+# Space Complexity: O(n)
 def sieve(n: int):
     # initialize all except 0 and 1 to True
     primes = [True if i > 1 else False for i in range(n + 1)] # iterate through all integers from 2 to sqrt(n)
