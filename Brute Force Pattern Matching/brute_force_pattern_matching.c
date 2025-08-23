70d35b80eef128995896b68f8211afeac02a43cf
Jacobus Burger <therealjacoburger@gmail.com>
2025-08-23 15:47:30 -0700

    starting Brute Force Pattern Matching (C99)

---
+++ Brute Force Pattern Matching/brute_force_pattern_matching.c
@@ -0,0 +1,14 @@
+/* Jacobus Burger (2025-08-23)
+ * Brute Force Pattern Matching (C99)
+ * Brute Force Pattern Matching is a brute force approach to matching
+ *      a substring of a larger string of contiguous data. It does so
+ *      by comparing the first letter of the substring with the current
+ *      letter in the string until it finds a match, then compares to
+ *      see that the substring and that section of the string both
+ *      match. This is an obvious approach, but isn't very effective
+ *      when compared to more clever approaches.
+ * Pattern Matching has a lot of great applications in multiple
+ *      disciplines, such as for bioinformatics.
+ * Info:
+ * - ...
+ */
