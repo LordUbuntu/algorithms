8fe1c3012f193b1571d9afe4472b3a9b310cd270
Jacobus Burger <therealjacoburger@gmail.com>
2025-08-29 18:23:35 -0700

    implement fibonacci function (C99)

--- Fibonacci Sequence/fibonacci_sequence.c
+++ Fibonacci Sequence/fibonacci_sequence.c
@@ -2,5 +2,15 @@
 
 int fibonacci(int n)
 {
-        return 1;
+        if (n <= 0) {
+                return 1;
+        }
+
+        int a = 1, b = 1, temp = 0;
+        for (size_t i = 0; i < n; i++) {
+                temp = b;
+                b = a + b;
+                a = temp;
+        }
+        return b;
 }
