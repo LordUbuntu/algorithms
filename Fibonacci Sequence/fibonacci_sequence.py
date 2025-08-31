347f3dd1adbd31f9b822e0d7cd7e933e778a15ef
Jacobus Burger <therealjacoburger@gmail.com>
2025-08-30 21:51:26 -0700

    expand description and make demos uniform

--- Fibonacci Sequence/fibonacci_sequence.py
+++ Fibonacci Sequence/fibonacci_sequence.py
@@ -2,15 +2,18 @@
 # Fibonacci Sequence (Python 3.13)
 # Description:
 # Fibonacci Sequence is a recursive mathematical sequence where every
-#   subsequent value Fn = Fn-1 + Fn-2. It often starts like
-#   1 1 2 3 5 8 13 ...
+#   subsequent value Fn = Fn-1 + Fn-2. It often starts like F0 = F1 = 1,
+#   interestingly it works bi-directionally in both positive and negative
+#   values for n.
+#                               F0 F1
+#   13, -8, 5, -3, 2, -1, 1, 0, 1  1  2 3 5 8 13 ...
 # Info:
 # - https://en.wikipedia.org/wiki/Fibonacci_sequence
 # - https://rosettacode.org/wiki/Fibonacci_sequence#Python
-from sys import argv
 
 
 def fibonacci(n: int) -> int:
+    # TODO: make work for all int
     if n <= 1:
         return 1
 
@@ -21,7 +24,5 @@ def fibonacci(n: int) -> int:
 
 
 if __name__ == "__main__":
-    if (len(argv) < 2):
-        exit(1)
-    n = int(argv[1])
+    n = int(input())
     print("fib {} = {}".format(n, fibonacci(n)))
