c72e272ca4fc7184f70ec16853b7258fcaa6307a
Jacobus Burger <therealjacoburger@gmail.com>
2025-08-30 21:51:08 -0700

    expand description and make demos uniform

--- Fibonacci Sequence/fibonacci_sequence.c
+++ Fibonacci Sequence/fibonacci_sequence.c
@@ -1,18 +1,20 @@
 /* Jacobus Burger (2025-08-29)
  * Fibonacci Sequence (C99)
- * Description:
  * Fibonacci Sequence is a recursive mathematical sequence where every
- *   subsequent value Fn = Fn-1 + Fn-2. It often starts like
- *   1 1 2 3 5 8 13 ...
+ *   subsequent value Fn = Fn-1 + Fn-2. It often starts like F0 = F1 = 1,
+ *   interestingly it works bi-directionally in both positive and
+ *   negative values for n.
+ *                               F0 F1
+ *   13, -8, 5, -3, 2, -1, 1, 0, 1  1  2 3 5 8 13 ...
  * Info:
  * - https://en.wikipedia.org/wiki/Fibonacci_sequence
  * - https://rosettacode.org/wiki/Fibonacci_sequence#C
  */
 #include <stdio.h>
-#include <stdlib.h>
 
 int fibonacci(int n)
 {
+        // TODO: make work for all int
         if (n <= 1) {
                 return 1;
         }
@@ -26,10 +28,9 @@ int fibonacci(int n)
         return b;
 }
 
-int main(int argc, char *argv[])
+int main(void)
 {
-        if (argc < 2)
-                return 1;
-        int n = atoi(argv[1]);
-        printf("fib n = %i\n", fibonacci(n));
+        int n;
+        scanf("%i", &n);
+        printf("fib %i = %i\n", n, fibonacci(n));
 }
