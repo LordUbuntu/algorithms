430efab5cb42bd08e98c73117300ffda5e301d59
Jacobus Burger <therealjacoburger@gmail.com>
2025-09-06 12:15:06 -0700

    clarity on how it works

--- Fibonacci Sequence/fibonacci_sequence.c
+++ Fibonacci Sequence/fibonacci_sequence.c
@@ -4,7 +4,7 @@
  *   subsequent value Fn = Fn-1 + Fn-2. It often starts like F0 = F1 = 1,
  *   interestingly it works bi-directionally in both positive and
  *   negative values for n.
- *                               F0 F1
+ *                            F0 F1
  *   13, -8, 5, -3, 2, -1, 1, 0, 1  1  2 3 5 8 13 ...
  * Info:
  * - https://en.wikipedia.org/wiki/Fibonacci_sequence
