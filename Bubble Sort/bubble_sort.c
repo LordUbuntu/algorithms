aad956af3448603a4e9f61e9fca8901d349154ee
Jacobus Burger <therealjacoburger@gmail.com>
2025-09-28 23:27:41 -0700

    attempt to correctly implement bubble sort (WIP)

--- Bubble Sort/bubble_sort.c
+++ Bubble Sort/bubble_sort.c
@@ -6,24 +6,28 @@
  *   easy to implement.
  * see:
  * - https://en.wikipedia.org/wiki/Bubble_sort
+ * - https://rosettacode.org/wiki/Sorting_algorithms/Bubble_sort#C
  */
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
-/* Time Complexity: O(n^2)
- * Space Complexity: O(1)
- */
 void sort(int *array, int length)
 {
-        for (int i = 1; i < length; i++) {
-                for (int j = 1; j < length; j++) {
-                        if (array[j - 1] >= array[j]) {
+        bool sorted = true;
+        while (sorted && length > 0) {
+                sorted = true;
+                for (int i = 1; i < length; i++) {
+                        if (array[i - 1] > array[i]) {
                                 // XOR swap
-                                array[j - 1] ^= array[j];
-                                array[j] ^= array[j - 1];
-                                array[j - 1] ^= array[j];
+                                array[i - 1] ^= array[i];
+                                array[i] ^= array[i - 1];
+                                array[i - 1] ^= array[i];
+                                // not sorted yet
+                                sorted = false;
                         }
                 }
+                length--;
         }
 }
 
